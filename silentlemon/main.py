52cbb76a04d893b026cd31a1ac6737de51ecb1bd
Akseli Palén <akseli.palen@gmail.com>
2017-11-25 22:09:06 +0200

    First working version

--- silentlemon/main.py
+++ silentlemon/main.py
@@ -5,59 +5,87 @@ import datetime
 import threading
 from math import floor
 from calendar_api import Calendar
+from lights import Lights
 
 MINUTE = 60
 HOUR = 60 * MINUTE
 TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
+DEVICE = 3
+
 DEFAULT_SETTING = {
     'start': {
         'fade_sec': 10,
-        'color_x': 0,
-        'color_y': 0,
-        'level': 100,
-        'devices': [3]
+        'color_x': 0.3,
+        'color_y': 0.3,
+        'level': 150,
+        'devices': [DEVICE]
     },
     'end': {
         'fade_sec': 10,
-        'color_x': 0,
-        'color_y': 0,
-        'level': 100,
-        'devices': [3]
+        'color_x': 0.3,
+        'color_y': 0.3,
+        'level': 0,
+        'devices': [DEVICE]
     }
 }
 
 KEYWORD_SETTING_MAP = {
-  'quick': {
+  'energizing': {
     'start': {
       'offset_sec': -HOUR / 2,
       'fade_sec': 10,
-      'color_x': 0,
-      'color_y': 0,
-      'level': 100,
-      'devices': [3]
+      'color_x': 0.25,
+      'color_y': 0.08,
+      'level': 200,
+      'devices': [DEVICE]
     },
     'end': {
-      'fade_duration': 1234,
-      'level': 0
+      'color_x': 0.25,
+      'color_y': 0.08,
+      'level': 100,
+      'devices': [DEVICE]
     }
   },
-  'creative': DEFAULT_SETTING,
+  'happy': {
+    'start': {
+      # Easter
+      'color_x': 0.2,
+      'color_y': 0.5,
+      'level': 200,
+      'devices': [DEVICE]
+    },
+    'end': {
+      'color_x': 0.3,
+      'color_y': 0.3,
+      'level': 50,
+      'devices': [DEVICE]
+    }
+  },
   'relaxed': DEFAULT_SETTING,
   'tasty': {
-    'offset_sec': -HOUR / 2,
-    'fade_sec': 10,
-    'color_x': 0,
-    'color_y': 0,
-    'level': 100,
-    'devices': [3]
+    'start': {
+        'color_x': 0,
+        'color_y': 0,
+        'level': 100,
+        'devices': [DEVICE]
+    },
+    'start': {
+        'color_x': 0,
+        'color_y': 0,
+        'level': 100,
+        'devices': [DEVICE]
+    }
   }
 }
 
+light_api = Lights()
 
 def find_setting(ev):
     wordlist = ev['description'].split()
-    for word in wordlist:
+    smallcaps = map(lambda x: x.lower(), wordlist)
+
+    for word in smallcaps:
         if word in KEYWORD_SETTING_MAP:
             return KEYWORD_SETTING_MAP[word]
     return DEFAULT_SETTING
@@ -66,10 +94,19 @@ def call_light(lc, dt):
     '''
     lc light call
     '''
+    setting = lc['setting']
+
     print(lc['type'] + ' ' + lc['event']['description'] + ' @' + str(dt) + 's')
 
+    for device_id in setting['devices']:
+        x = setting['color_x']
+        y = setting['color_y']
+        l = setting['level']
+        light_api.setLights(x, y, l, device_id)
+        print('  Device {0}: ({1}, {2}) {3}%'.format(device_id, x, y, l))
+
 def schedule(light_call):
-    now_time = time.time()
+    now_time = 1511674500
     delta_seconds = light_call['at_time'] - now_time
     # Make quick
     dt = delta_seconds / 1000.0
