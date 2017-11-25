865858a7bcb81ad1ae87827a03e933007170a258
mourula <tiina.ristila@intopalo.com>
2017-11-25 18:39:21 +0200

    Add init parameters to the class
    Init parameters were created for the class. Otherwise the lightsOff method turned the color blue.

--- silentlemon/Lights.py
+++ silentlemon/Lights.py
@@ -3,17 +3,21 @@ import requests
 
 class Lights:
     
+    def __init__(self):
+        self._colorX = 0
+        self._colorY = 0
+    
     ## Turn on selected lights
     def setLights(self, colorX, colorY, level, deviceId=-1):
         ## Set parameters
-        colorX = int(colorX*32768)
-        colorY = int(colorY*32768) 
+        self._colorX = int(colorX*32768)
+        self._colorY = int(colorY*32768) 
         
         ## If deviceID is 1,2,3 then set it, otherwise leave it from the parameters and all of the lights are set
         if (deviceId == 0 or deviceId == 3 or deviceId == 8 ):
-                payload = {'device': deviceId, 'level': level, 'colour_x': colorX, 'colour_y': colorY}
+                payload = {'device': deviceId, 'level': level, 'colour_x': self._colorX, 'colour_y': self._colorY}
         else:
-                payload = {'level': level, 'colour_x': colorX, 'colour_y': colorY}
+                payload = {'level': level, 'colour_x': self._colorX, 'colour_y': self._colorY}
         
         ## Send the request to change the lights
         request = requests.get('https://5nk8a0rfn5.execute-api.eu-west-1.amazonaws.com/v1/command', params=payload)
@@ -21,10 +25,8 @@ class Lights:
      ## Turn off all of the lights   
     def lightsOff(self):
         ## Set the parameters
-        colorX = 0 # 0-1
-        colorY = 0 # 0-1
         level = 0 # 0-100
         
         ## Send the request to turn off the lights
-        payload = {'level': level, 'colour_x': colorX, 'colour_y': colorY}
+        payload = {'level': level, 'colour_x': self._colorX, 'colour_y': self._colorY}
         request = requests.get('https://5nk8a0rfn5.execute-api.eu-west-1.amazonaws.com/v1/command', params=payload)
